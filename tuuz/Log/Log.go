a8dd5acc59829fabfe538e8359957a885ab5b57f
Tuuz <2539@aerofsx.com>
2020-03-25 16:34:29 +0800

    Tuuz

--- tuuz/Log/Log.go
+++ tuuz/Log/Log.go
@@ -6,45 +6,43 @@ import (
 	"os"
 )
 
-var logger *log.Logger
-var file *os.File
-
-func Write(file_name string, logs string, discript string, other interface{}) {
-	file, err := os.OpenFile("log/"+file_name+".log", os.O_APPEND|os.O_CREATE, 666)
+func Write(file_name string, logs string, discript string, other string) {
+	file, err := os.OpenFile("log/"+file_name+".log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 777)
 	if err != nil {
-		log.Fatalln("fail to create log/" + file_name + ".log file!")
+		log.Fatalln(err)
 	} else {
-		logger = log.New(file, "", log.LstdFlags)
+		logger := log.New(file, "", log.LstdFlags)
 		logger.Println(logs, discript, other)
+		file.Close()
 	}
 }
 
 func Error(file_name string, err error) {
-	Write(file_name, "", "", err)
+	Write(file_name, "", "", err.Error())
 }
 
 func Err(err error) {
-	Write("Error", "", "", err)
+	Write("Error", "", "", err.Error())
 }
 
 func Errs(err error, log string) {
-	Write("Error", log, "", err)
+	Write("Error", log, "", err.Error())
 }
 
 //Database err
 func Drr(err error) {
-	Write("Database", "", "", err)
+	Write("Database", "", "", err.Error())
 }
 
-func Crr(logs interface{}) {
-	Write("Common", "", "", logs)
+func Crr(logs error) {
+	Write("Common", "", "", logs.Error())
 }
 
-func Crrs(logs interface{}, discript string) {
-	Write("Common", "", discript, logs)
+func Crrs(logs error, discript string) {
+	Write("Common", "", discript, logs.Error())
 }
 
 func Dbrr(err error, log string) {
-	fmt.Println(err)
-	Write("Dberror", log, "", err)
+	fmt.Println(err.Error())
+	Write("Dberror", log, "", err.Error())
 }
