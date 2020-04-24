f74a4b3d19464ba1ce9d2f0012fa29d545d6467d
Tuuz <2539@aerofsx.com>
2020-04-24 16:01:12 +0800

    Tuuz

--- tuuz/Input/Post.go
+++ tuuz/Input/Post.go
@@ -2,6 +2,7 @@ package Input
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/tuuz/Calc"
 	"main.go/tuuz/RET"
 )
 
@@ -13,5 +14,56 @@ func Post(key string, c *gin.Context, xss bool) (string, bool) {
 		return "", false
 	} else {
 		return in, true
+	}
+}
+
+func PostInt(key string, c *gin.Context) (int, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		c.JSON(200, RET.Ret_fail(400, key))
+		c.Abort()
+		return 0, false
+	} else {
+		i, e := Calc.String2Int(in)
+		if e != nil {
+			c.JSON(200, RET.Ret_fail(407, key+" should be int"))
+			c.Abort()
+			return 0, false
+		}
+		return i, true
+	}
+}
+
+func PostInt64(key string, c *gin.Context) (int64, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		c.JSON(200, RET.Ret_fail(400, key))
+		c.Abort()
+		return 0, false
+	} else {
+		i, e := Calc.String2Int64(in)
+		if e != nil {
+			c.JSON(200, RET.Ret_fail(407, key+" should be int64"))
+			c.Abort()
+			return 0, false
+		}
+		return i, true
+	}
+}
+
+func PostFloat64(key string, c *gin.Context) (float64, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		c.JSON(200, RET.Ret_fail(400, key))
+		c.Abort()
+		return 0, false
+	} else {
+		i, e := Calc.String2Float64(in)
+		if e != nil {
+			c.JSON(200, RET.Ret_fail(407, key+" should be float64"))
+			c.Abort()
+			return 0, false
+		}
+		return i, true
 	}
 }
