c7f4410d32e3b5417218ae0819a74e8a8d48ff09
Tuuz <2539@aerofsx.com>
2020-08-19 16:03:08 +0800

    Tuuz

--- tuuz/Calc/Token.go
+++ tuuz/Calc/Token.go
@@ -11,3 +11,9 @@ func GenerateToken() string {
 	str := strconv.FormatInt(unix, 10) + strconv.FormatInt(int64(rand), 10)
 	return Md5(str)
 }
+
+func GenerateOrderId() string {
+	rand := Mt_rand(0, 99999999)
+	str := Int642String(time.Now().UnixNano())
+	return time.Now().Format("D20060102T15:04:05U" + str + Int642String(rand))
+}
