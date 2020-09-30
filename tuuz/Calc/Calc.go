eb8df43e994ad0551a987265c2f260e98a0099db
Tuuz <miracle-plus@outlook.com>
2020-09-30 11:16:49 +0800

    Tuuz

--- tuuz/Calc/Calc.go
+++ tuuz/Calc/Calc.go
@@ -2,7 +2,6 @@ package Calc
 
 import (
 	"crypto/rand"
-	"github.com/shopspring/decimal"
 	"math"
 	"math/big"
 	rand2 "math/rand"
@@ -108,69 +107,4 @@ func Round(x float64, decimal int) float64 {
 func Decimal(x float64, decimal int) string {
 	value := strconv.FormatFloat(x, 'f', decimal, 64)
 	return value
-}
-
-func todecimal(number interface{}) decimal.Decimal {
-	switch number.(type) {
-	case int:
-		return decimal.NewFromInt(int64(number.(int)))
-
-	case int64:
-		return decimal.NewFromInt(number.(int64))
-
-	case float64:
-		return decimal.NewFromFloat(number.(float64))
-
-	case float32:
-		return decimal.NewFromFloat32(number.(float32))
-
-	case int32:
-		return decimal.NewFromInt32(number.(int32))
-
-	case string:
-		ret, err := decimal.NewFromString(number.(string))
-		if err != nil {
-			return decimal.NewFromInt(0)
-		}
-		return ret
-
-	default:
-		return decimal.NewFromFloat(Any2Float64(number))
-	}
-}
-
-func Bc_sum(num1, num2 interface{}) decimal.Decimal {
-	return todecimal(num1).Add(todecimal(num2))
-}
-
-func Bc_min(num1, num2 interface{}) decimal.Decimal {
-	return todecimal(num1).Sub(todecimal(num2))
-}
-
-func Bc_mul(num1, num2 interface{}) decimal.Decimal {
-	return todecimal(num1).Mul(todecimal(num2))
-}
-
-func Bc_div(num1, num2 interface{}) decimal.Decimal {
-	return todecimal(num1).Div(todecimal(num2))
-}
-
-func Bc_pow(num1, num2 interface{}) decimal.Decimal {
-	return todecimal(num1).Pow(todecimal(num2))
-}
-
-func Bc_round(num1 interface{}, round int) decimal.Decimal {
-	return todecimal(num1).Round(int32(round))
-}
-
-func Bc_div_round(num1 interface{}, num2 interface{}, round int) decimal.Decimal {
-	return todecimal(num1).DivRound(todecimal(num2), int32(round))
-}
-
-func Bc_abs(num1 interface{}) decimal.Decimal {
-	return todecimal(num1).Abs()
-}
-
-func Bc_mod(num1, num2 interface{}) decimal.Decimal {
-	return todecimal(num1).Mod(todecimal(num2))
 }
