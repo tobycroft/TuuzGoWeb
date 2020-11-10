8f8e160a47454e897f0b18438ae67268bb8b8b05
Tuuz <miracle-plus@outlook.com>
2020-11-10 15:15:09 +0800

    Tuuz

--- tuuz/Array/Array.go
+++ tuuz/Array/Array.go
@@ -175,3 +175,35 @@ func ArrayUnique_string(slice []string) []string {
 	}
 	return n64
 }
+
+func Array_find_max_int64(slice []int64) int64 {
+	if len(slice) < 1 {
+		return 0
+	}
+	maxVal := slice[0]
+	maxIndex := 0
+	for i := 1; i < len(slice); i++ {
+		//从第二个 元素开始循环比较，如果发现有更大的，则交换
+		if maxVal < slice[i] {
+			maxVal = slice[i]
+			maxIndex = i
+		}
+	}
+	return slice[maxIndex]
+}
+
+func Array_find_max_float64(slice []float64) float64 {
+	if len(slice) < 1 {
+		return 0
+	}
+	maxVal := slice[0]
+	maxIndex := 0
+	for i := 1; i < len(slice); i++ {
+		//从第二个 元素开始循环比较，如果发现有更大的，则交换
+		if maxVal < slice[i] {
+			maxVal = slice[i]
+			maxIndex = i
+		}
+	}
+	return slice[maxIndex]
+}
