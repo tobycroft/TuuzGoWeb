3cb62c5021c9a2b02275a16e3ce044917f0b7104
Tuuz <miracle-plus@outlook.com>
2020-11-10 15:24:11 +0800

    Tuuz

--- tuuz/Array/Array.go
+++ tuuz/Array/Array.go
@@ -207,3 +207,54 @@ func Array_find_max_float64(slice []float64) float64 {
 	}
 	return slice[maxIndex]
 }
+
+func Array_diff_float64(slice, slice2 []float64) []float64 {
+	n64 := []float64{}
+	for _, s1 := range slice {
+		temp := true
+		for _, s2 := range slice2 {
+			if s2 == s1 {
+				temp = false
+				break
+			}
+		}
+		if temp {
+			n64 = append(n64, s1)
+		}
+	}
+	return n64
+}
+
+func Array_diff_int64(slice, slice2 []int64) []int64 {
+	n64 := []int64{}
+	for _, s1 := range slice {
+		temp := true
+		for _, s2 := range slice2 {
+			if s2 == s1 {
+				temp = false
+				break
+			}
+		}
+		if temp {
+			n64 = append(n64, s1)
+		}
+	}
+	return n64
+}
+
+func Array_diff_string(slice, slice2 []string) []string {
+	n64 := []string{}
+	for _, s1 := range slice {
+		temp := true
+		for _, s2 := range slice2 {
+			if s2 == s1 {
+				temp = false
+				break
+			}
+		}
+		if temp {
+			n64 = append(n64, s1)
+		}
+	}
+	return n64
+}
