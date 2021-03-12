ccde51382ec63f95add74fbd95b2911e16af28f9
Tuuz <silverhawk@foxmail.com>
2021-03-12 11:44:22 +0800

    Tuuz

--- app/v1/index/controller/login.go
+++ app/v1/index/controller/login.go
@@ -7,46 +7,16 @@ import (
 	"image/png"
 	"main.go/tuuz/Base64"
 	"main.go/tuuz/Captcha"
+	"main.go/tuuz/Input"
 	"main.go/tuuz/RET"
 )
 
-var username string
-var password string
-var verify string
-var is_verify bool
-var is_captcha bool
-var captcha string
-
 func LoginController(route *gin.RouterGroup) {
 	route.Use(cors.Default())
-	route.Use(initialize(), gin.Recovery())
 
 	route.Any("/get_captcha", get_captcha)
 	route.Any("/get_captcha_64", get_captcha_64)
 	route.Any("/verify_captcha", verify_captcha)
-}
-
-func initialize() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		var is bool
-		username, is = c.GetPostForm("username")
-		if is == false {
-			username, is = c.GetQuery("username")
-			if is == false {
-				RET.Fail(c, 400, nil, "username")
-				return
-			}
-		}
-		if len(username) < 8 {
-			RET.Fail(c, 400, nil, "username<8")
-			return
-		}
-		password, is = c.GetPostForm("password")
-		verify, is_verify = c.GetPostForm("verify")
-		captcha, is_captcha = c.GetPostForm("captcha")
-		c.Next()
-	}
 }
 
 func get_captcha(c *gin.Context) {
@@ -77,8 +47,8 @@ func verify_captcha(c *gin.Context) {
 		RET.Fail(c, 400, nil, "ident")
 		return
 	}
-	if is_verify == false {
-		RET.Fail(c, 400, nil, "is_verify")
+	verify, ok := Input.Post("verify", c, false)
+	if !ok {
 		return
 	}
 	bol := Captcha.AutoVerify(ident, verify)
