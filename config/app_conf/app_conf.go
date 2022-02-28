4ad498a433ff39bde7a99cd097e3f412a90a6136
Tuuz <silverhawk@foxmail.com>
2022-02-28 11:26:22 +0800

    Tuuz

--- config/app_conf/app_conf.go
+++ config/app_conf/app_conf.go
@@ -5,3 +5,5 @@ const Debug = "false"
 const TestMode = false
 const AppMode = "debug"
 const WebsocketKey = ""
+
+const SecureJsonPrefix = ""
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/config/app_conf"
 	"main.go/route"
 )
 
@@ -11,7 +12,7 @@ func main() {
 	//gin.SetMode(gin.ReleaseMode)
 	//gin.DefaultWriter = ioutil.Discard
 	mainroute.SetTrustedProxies(nil)
-	mainroute.SecureJsonPrefix("")
+	mainroute.SecureJsonPrefix(app_conf.SecureJsonPrefix)
 	route.OnRoute(mainroute)
 	mainroute.Run(":80")
 
