ed91bd172b00fe4d3c5fe200be7f31e9f75ea28f
Tuuz <silverhawk@foxmail.com>
2022-02-28 15:14:32 +0800

    新增文件上传方法，增加文件上传等控制，文件上传到的文件夹，文件夹存储日期等方式

--- config/app_conf/app_conf.go
+++ config/app_conf/app_conf.go
@@ -10,6 +10,6 @@ const SecureJson = false
 const SecureJsonPrefix = "tuuzgoweb"
 
 const FileSavePath = "public"
-const FilePathCreateByDate = true
-const FilePathCreateByDay = true
-const FileNameSecurity = true //this will turn original file name into MD5 to avoid path attack
+const FilePathCreateByDay = true  //create day's privilige is higher than date, if this on the following Date will be no longer avail
+const FilePathCreateByDate = true //if you want to save the file into current month's folder, make sure you have turn this on and deactivated the CreateByDay setting
+const FileNameSecurity = true     //this will turn original file name into MD5 to avoid path attack
