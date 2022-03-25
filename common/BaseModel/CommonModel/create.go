487b57312139ccf2587bb376607c4a0bdaadfbf0
Tuuz <silverhawk@foxmail.com>
2022-03-25 10:17:57 +0800

    增加CommonModel快速编辑功能

---
+++ common/BaseModel/CommonModel/create.go
@@ -0,0 +1,36 @@
+package CommonModel
+
+import (
+	"github.com/tobycroft/gorose-pro"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+type Interface struct {
+	Table string
+	Db    gorose.IOrm
+}
+
+func (self *Interface) Create(data map[string]interface{}) int64 {
+	db := self.Db.Table(self.Table)
+	db.Data(data)
+	ret, err := db.InsertGetId()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return 0
+	} else {
+		return ret
+	}
+}
+
+func Create(table string, data map[string]interface{}) int64 {
+	db := tuuz.Db().Table(table)
+	db.Data(data)
+	ret, err := db.InsertGetId()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return 0
+	} else {
+		return ret
+	}
+}
