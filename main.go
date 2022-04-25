ca15c6ebc397db8982431e538e503acef893b7b0
Tuuz <silverhawk@foxmail.com>
2022-04-25 17:58:27 +0800

    修改订单系统，订单长度限制与腾讯相同

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"main.go/config/app_conf"
 	"main.go/route"
+	"main.go/tuuz/Calc"
 )
 
 func main() {
-
+	Calc.RefreshBaseNum()
 	mainroute := gin.Default()
 	//gin.SetMode(gin.ReleaseMode)
 	//gin.DefaultWriter = ioutil.Discard
