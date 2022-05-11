953d36cefa07845926c00a8478d46ceca7deb1bb
Tuuz <silverhawk@foxmail.com>
2022-05-11 15:00:14 +0800

    跨域类修改

--- common/BaseController/common.go
+++ common/BaseController/common.go
@@ -13,10 +13,6 @@ func CommonController() gin.HandlerFunc {
 			c.AbortWithStatus(http.StatusNoContent)
 			return
 		}
-		//	//c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
-		//	//c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
-		//	//c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		//	//c.Header("Access-Control-Allow-Credentials", "true")
 		c.Next()
 	}
 }
