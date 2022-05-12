d1e6bab17d0115ff73553137927d31ae2a26f61e
Tuuz <silverhawk@foxmail.com>
2022-05-12 12:36:13 +0800

    短信类修改

--- extend/ASMS/ASMSModel.go
+++ extend/ASMS/ASMSModel.go
@@ -40,6 +40,23 @@ func Api_find(phone, code interface{}) gorose.Data {
 	}
 }
 
+func Api_find_in10(phone, code interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"phone": phone,
+		"code":  code,
+	}
+	db.Where(where)
+	db.Where("date>Date_SUB(NOW(),INTERVAL 10 MINUTE)")
+	ret, err := db.Find()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
 func Api_find_in5(phone, code interface{}) gorose.Data {
 	db := tuuz.Db().Table(table)
 	where := map[string]interface{}{
