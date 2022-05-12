8364c1274bc6cc66b3503470e5ef006fa4c41ab3
Tuuz <silverhawk@foxmail.com>
2022-05-12 12:09:03 +0800

    跨域类修改

--- extend/ASMS/ASMS.go
+++ extend/ASMS/ASMS.go
@@ -2,6 +2,7 @@ package ASMS
 
 import (
 	"errors"
+	"github.com/Unknwon/goconfig"
 	"main.go/tuuz/Calc"
 	"main.go/tuuz/Jsong"
 	"main.go/tuuz/Net"
@@ -18,12 +19,19 @@ import (
  *
  */
 
-const name = "test"
-const token = "test"
-
 const url = "http://asms.tuuz.cc:10081"
 
 func Sms_single(phone interface{}, quhao, code interface{}) error {
+	conf, err := goconfig.LoadConfigFile("conf.ini")
+	if err != nil {
+		return err
+	}
+	value, err := conf.GetSection("wechat")
+	if err != nil {
+		return err
+	}
+	name := value["name"]
+	token := value["token"]
 	ts := time.Now().Unix()
 	param := map[string]interface{}{
 		"phone": phone,
