ade6de9d74bafbeaca9a3cd868266900daf7b732
火线兔 <silverhawk@foxmail.com>
2022-06-16 09:40:17 +0800

    时间处理更加宽容

--- tuuz/Input/Post.go
+++ tuuz/Input/Post.go
@@ -117,13 +117,20 @@ func PostDateTime(key string, c *gin.Context) (time.Time, bool) {
 		return time.Time{}, false
 	} else {
 		p, err := time.Parse("2006-01-02 15:04:05", in)
-		if err != nil {
-			c.JSON(RET.Ret_fail(407, err.Error(), key+" should only be a DateTime"))
-			c.Abort()
-			return time.Time{}, false
-		} else {
+		if err == nil {
 			return p, true
 		}
+		p, err = time.Parse(time.RFC3339, in)
+		if err == nil {
+			return p, true
+		}
+		p, err = time.Parse(time.RFC3339Nano, in)
+		if err == nil {
+			return p, true
+		}
+		c.JSON(RET.Ret_fail(407, err.Error(), key+" should only be a DateTime or RFC3339"))
+		c.Abort()
+		return time.Time{}, false
 	}
 }
 
