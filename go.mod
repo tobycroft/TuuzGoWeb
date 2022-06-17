760a60f502a828307d093675201d287b350119e1
火线兔 <silverhawk@foxmail.com>
2022-06-17 15:39:38 +0800

    新增弱验证Post模式，不再自动控制错误返回，方便适用更多场景

--- go.mod
+++ go.mod
@@ -7,14 +7,14 @@ require (
 	github.com/afocus/captcha v0.0.0-20191010092841-4bd1f21c8868
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/gin-contrib/cors v1.3.1
-	github.com/gin-gonic/gin v1.7.7
+	github.com/gin-gonic/gin v1.8.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gomodule/redigo v1.8.8
 	github.com/gorilla/websocket v1.5.0
 	github.com/json-iterator/go v1.1.12
 	github.com/kirinlabs/HttpRequest v1.1.1
 	github.com/shopspring/decimal v1.3.1
-	github.com/tobycroft/gorose-pro v1.2.9
+	github.com/tobycroft/gorose-pro v1.4.1
 )
 
 require (
@@ -23,6 +23,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/gohouse/golib v0.0.0-20210711163732-a5c22059eb75 // indirect
 	github.com/gohouse/t v0.0.0-20201007094014-630049a6bfe9 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
@@ -31,11 +32,13 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	github.com/smartystreets/goconvey v1.7.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167 // indirect
-	golang.org/x/image v0.0.0-20220413100746-70e8d0d3baa9 // indirect
-	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/image v0.0.0-20220617043117-41969df76e82 // indirect
+	golang.org/x/net v0.0.0-20220615171555-694bf12d69de // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ tuuz/Input/SliencerPost.go
@@ -0,0 +1,349 @@
+package Input
+
+import (
+	"crypto/sha256"
+	"errors"
+	"github.com/gin-gonic/gin"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/shopspring/decimal"
+	"html/template"
+	"io"
+	"main.go/config/app_conf"
+	"main.go/tuuz/Array"
+	"main.go/tuuz/Calc"
+	"main.go/tuuz/Date"
+	"main.go/tuuz/Jsong"
+	"main.go/tuuz/RET"
+	"main.go/tuuz/Vali"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+func SPost(key string, c *gin.Context, xss bool) (string, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return "", false
+	} else {
+		if xss {
+			return template.JSEscapeString(in), true
+		} else {
+			return in, true
+		}
+	}
+}
+
+func SPostPhone(key string, length int, c *gin.Context) (string, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return "", false
+	} else {
+		ret, err := decimal.NewFromString(in)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, key+" should only be numbers", key+" should only be numbers"))
+			c.Abort()
+			return "", false
+		}
+		err = Vali.Length(ret.String(), length, length)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, key+" "+err.Error(), key+" "+err.Error()))
+			c.Abort()
+			return "", false
+		}
+		return ret.String(), true
+	}
+}
+
+func SPostDate(key string, c *gin.Context) (time.Time, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return time.Time{}, false
+	} else {
+		p, err := time.Parse("2006-01-02", in)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, err.Error(), key+" should only be a Date"))
+			c.Abort()
+			return time.Time{}, false
+		} else {
+			return p, true
+		}
+	}
+}
+
+func SPostDateTime(key string, c *gin.Context) (time.Time, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return time.Time{}, false
+	} else {
+		p, err := time.Parse("2006-01-02 15:04:05", in)
+		if err == nil {
+			return p, true
+		}
+		p, err = time.Parse(time.RFC3339, in)
+		if err == nil {
+			return p, true
+		}
+		p, err = time.Parse(time.RFC3339Nano, in)
+		if err == nil {
+			return p, true
+		}
+		c.JSON(RET.Ret_fail(407, err.Error(), key+" should only be a DateTime or RFC3339"))
+		c.Abort()
+		return time.Time{}, false
+	}
+}
+
+func SPostTime(key string, c *gin.Context) (time.Time, bool) {
+	in, ok := PostInt64(key, c)
+	if !ok {
+		return time.Time{}, false
+	} else {
+		return time.Unix(in, 0), true
+	}
+}
+
+func SPostLength(key string, min, max int, c *gin.Context, xss bool) (string, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return "", false
+	} else {
+		err := Vali.Length(in, min, max)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, key+" "+err.Error(), key+" "+err.Error()))
+			c.Abort()
+			return "", false
+		}
+		if xss {
+			return template.JSEscapeString(in), true
+		} else {
+			return in, true
+		}
+	}
+}
+
+func SPostInt(key string, c *gin.Context) (int, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return 0, false
+	} else {
+		i, e := Calc.String2Int(in)
+		if e != nil {
+			c.JSON(RET.Ret_fail(407, e.Error(), key+" should be int"))
+			c.Abort()
+			return 0, false
+		}
+		return i, true
+	}
+}
+
+func SPostInt64(key string, c *gin.Context) (int64, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return 0, false
+	} else {
+		i, e := Calc.String2Int64(in)
+		if e != nil {
+			c.JSON(RET.Ret_fail(407, e.Error(), key+" should be int64"))
+			c.Abort()
+			return 0, false
+		}
+		return i, true
+	}
+}
+
+func SPostFloat64(key string, c *gin.Context) (float64, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return 0, false
+	} else {
+		i, e := Calc.String2Float64(in)
+		if e != nil {
+			c.JSON(RET.Ret_fail(407, e.Error(), key+" should be float64"))
+			c.Abort()
+			return 0, false
+		}
+		return i, true
+	}
+}
+
+func SPostDecimal(key string, c *gin.Context) (decimal.Decimal, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return decimal.Zero, false
+	} else {
+		ret, err := decimal.NewFromString(in)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, err.Error(), key+" should be a Number"))
+			c.Abort()
+			return decimal.Zero, false
+		}
+		return ret, true
+	}
+}
+
+func SPostBool(key string, c *gin.Context) (bool, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return false, false
+	} else {
+		switch in {
+		case "1":
+			return true, true
+
+		case "0":
+			return false, true
+
+		case "true":
+			return true, true
+
+		case "false":
+			return false, true
+
+		default:
+			c.JSON(RET.Ret_fail(407, key+" should be Boolean", key+" should be Boolean"))
+			c.Abort()
+			return false, false
+		}
+	}
+}
+
+func SPostArray(key string, c *gin.Context) ([]interface{}, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return nil, false
+	} else {
+		i, err := Jsong.JArray(in)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, err.Error(), key+" should be a Json-Array"))
+			c.Abort()
+			return nil, false
+		}
+		return i, true
+	}
+}
+
+func SPostObject(key string, c *gin.Context) (map[string]interface{}, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return nil, false
+	} else {
+		i, err := Jsong.JObject(in)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, key+" should be a Json-Object", key+" should be a Json-Object"))
+			c.Abort()
+			return nil, false
+		}
+		return i, true
+	}
+}
+
+func SPostArrayObject(key string, c *gin.Context) ([]map[string]interface{}, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return nil, false
+	} else {
+		i, err := Jsong.JArrayObject(in)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, key+" should be a Json-ArrayObject", key+" should be a Json-ArrayObject"))
+			c.Abort()
+			return nil, false
+		}
+		return i, true
+	}
+}
+
+func SPostAny(key string, c *gin.Context, AnyType interface{}) bool {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return false
+	} else {
+		err := jsoniter.UnmarshalFromString(in, &AnyType)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, err.Error(), key+" should be a Json-AnyType"))
+			c.Abort()
+			return false
+		}
+		return true
+	}
+}
+
+func SPostLimitPage(c *gin.Context) (int, int, error) {
+	limit, ok := SPostInt("limit", c)
+	if !ok {
+		return 0, 0, errors.New("limit")
+	}
+	page, ok := SPostInt("page", c)
+	if !ok {
+		return 0, 0, errors.New("page")
+	}
+	return limit, page, nil
+}
+
+func SPostIn(key string, c *gin.Context, str_slices []string) (string, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		return "", false
+	} else {
+		if Array.InArrayString(in, str_slices) {
+			return in, true
+		} else {
+			c.JSON(RET.Ret_fail(407, key+" 's data should in ["+strings.Join(str_slices, ",")+"]", key+" 's data should in ["+strings.Join(str_slices, ",")+"]"))
+			c.Abort()
+			return in, false
+		}
+	}
+}
+
+func SUpload(c *gin.Context) (File, bool) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return File{}, false
+	}
+	temp_file, err := file.Open()
+	defer temp_file.Close()
+	if err != nil {
+		c.JSON(RET.Ret_fail(406, "File Open Error", "POST-[file]:"+err.Error()))
+		c.Abort()
+		return File{}, false
+	}
+	file_hash := sha256.New()
+	if _, err := io.Copy(file_hash, temp_file); err != nil {
+		c.JSON(RET.Ret_fail(303, "File Hash Error", "POST-[file]:"+err.Error()))
+		c.Abort()
+		return File{}, false
+	}
+	file_md5 := file_hash.Sum(nil)
+	filename := filepath.Base(file.Filename)
+	ext := filepath.Ext(file.Filename)
+	var path string
+	if app_conf.FilePathCreateByDay {
+		path = app_conf.FileSavePath + "/" + Date.TodayCombine() + "/"
+	} else if app_conf.FilePathCreateByDate {
+		path = app_conf.FileSavePath + "/" + Date.ThisMonthCombine() + "/"
+	} else {
+		path = app_conf.FileSavePath + "/"
+	}
+	err = pathmake(path)
+	if err != nil {
+		c.JSON(RET.Ret_fail(500, "Create Path Fail", "POST-[file]:"+err.Error()))
+		c.Abort()
+		return File{}, false
+	}
+	if app_conf.FileNameSecurity {
+		filename = Calc.Md5(filename)
+	}
+	err = c.SaveUploadedFile(file, path+filename+ext)
+	if err != nil {
+		c.JSON(RET.Ret_fail(500, "File Saved Fail", "POST-[file]:"+err.Error()))
+		c.Abort()
+		return File{}, false
+	}
+	return File{
+		Path:     path + "/" + filename,
+		FileName: filename,
+		Size:     file.Size,
+		Md5:      string(file_md5),
+		Mime:     file.Header.Get("Content-Type"),
+		Ext:      ext,
+	}, true
+}
