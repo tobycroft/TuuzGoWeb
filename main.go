bb4417ed2abfe9d4fb1633845b5656f6e51d2357
火线兔 <silverhawk@foxmail.com>
2022-06-22 13:36:03 +0800

    修改MPost

--- main.go
+++ main.go
@@ -1,30 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/config/app_conf"
+	"main.go/route"
+	"main.go/tuuz/Calc"
+)
 
 func main() {
 
-	arr := map[string]interface{}{
-		"aaa": "bbb",
-		"ccc": "ddd",
-		"eee": nil,
-	}
-	ass(&arr)
-	fmt.Println(arr)
+	Calc.RefreshBaseNum()
+	mainroute := gin.Default()
+	//gin.SetMode(gin.ReleaseMode)
+	//gin.DefaultWriter = ioutil.Discard
+	mainroute.SetTrustedProxies([]string{"0.0.0.0/0"})
+	mainroute.SecureJsonPrefix(app_conf.SecureJsonPrefix)
+	route.OnRoute(mainroute)
+	mainroute.Run(":80")
 
-	//Calc.RefreshBaseNum()
-	//mainroute := gin.Default()
-	////gin.SetMode(gin.ReleaseMode)
-	////gin.DefaultWriter = ioutil.Discard
-	//mainroute.SetTrustedProxies([]string{"0.0.0.0/0"})
-	//mainroute.SecureJsonPrefix(app_conf.SecureJsonPrefix)
-	//route.OnRoute(mainroute)
-	//mainroute.Run(":80")
-
-}
-
-func ass(arr *map[string]interface{}) {
-	temp := *arr
-	temp["eee"] = "fff"
-	arr = &temp
 }
