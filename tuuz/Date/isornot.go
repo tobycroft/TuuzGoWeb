53da98bde6ba7961b3153aaa7501803d8d88958d
火线兔 <silverhawk@foxmail.com>
2022-07-01 18:10:52 +0800

    Date功能丰富时间比对功能

--- tuuz/Date/isornot.go
+++ tuuz/Date/isornot.go
@@ -13,15 +13,11 @@ func Date_between_time(the_time time.Time, start_time time.Time, end_time time.T
 }
 
 func Date_is_in_this_Month(the_time time.Time) bool {
-	this_month := Date_offset_month1st(0)
-	next_month := Date_offset_month1st(1)
-	return Date_between_time(the_time, this_month, next_month)
+	return Date_is_in_offset_free_Month(the_time, 0, 1)
 }
 
 func Date_is_in_offset_Month(the_time time.Time, offset int) bool {
-	this_month := Date_offset_month1st(offset)
-	next_month := Date_offset_month1st(offset + 1)
-	return Date_between_time(the_time, this_month, next_month)
+	return Date_is_in_offset_free_Month(the_time, offset, offset+1)
 }
 
 func Date_is_in_offset_free_Month(the_time time.Time, offset_start, offset_end int) bool {
@@ -31,15 +27,12 @@ func Date_is_in_offset_free_Month(the_time time.Time, offset_start, offset_end i
 }
 
 func Date_is_in_this_Week(the_time time.Time) bool {
-	this_week := Date_offset_week1st(0)
-	next_week := Date_offset_week1st(1)
-	return Date_between_time(the_time, this_week, next_week)
+	return Date_is_in_offset_free_Week(the_time, 0, 1)
+
 }
 
 func Date_is_in_offset_Week(the_time time.Time, offset int) bool {
-	this_week := Date_offset_week1st(offset)
-	next_week := Date_offset_week1st(offset + 1)
-	return Date_between_time(the_time, this_week, next_week)
+	return Date_is_in_offset_free_Week(the_time, offset, offset+1)
 }
 
 func Date_is_in_offset_free_Week(the_time time.Time, offset_start, offset_end int) bool {
@@ -49,15 +42,11 @@ func Date_is_in_offset_free_Week(the_time time.Time, offset_start, offset_end in
 }
 
 func Date_is_in_today(the_time time.Time) bool {
-	today := Date_offset_dayZero(0)
-	tomorrow := Date_offset_dayZero(1)
-	return Date_between_time(the_time, today, tomorrow)
+	return Date_is_in_offset_free_Day(the_time, 0, 1)
 }
 
 func Date_is_in_offset_Day(the_time time.Time, offset int) bool {
-	today := Date_offset_dayZero(offset)
-	tomorrow := Date_offset_dayZero(offset + 1)
-	return Date_between_time(the_time, today, tomorrow)
+	return Date_is_in_offset_free_Day(the_time, offset, offset+1)
 }
 
 func Date_is_in_offset_free_Day(the_time time.Time, offset_start, offset_end int) bool {
