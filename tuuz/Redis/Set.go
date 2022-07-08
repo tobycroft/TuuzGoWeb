b3809dafcf66e3a4afe2fa0eec10e805fbb95cf6
火线兔 <silverhawk@foxmail.com>
2022-07-08 18:30:52 +0800

    重构Redis模块

--- tuuz/Redis/Set.go
+++ tuuz/Redis/Set.go
@@ -1,11 +1,16 @@
 package Redis
 
 import (
+	"github.com/shopspring/decimal"
 	"main.go/config/app_conf"
 	"time"
 )
 
-func SAdd(key string, value string, duration time.Duration) error {
+func SAdd(key string, value any, duration time.Duration) error {
+	return goredis.SAdd(goredis_ctx, app_conf.Project+":"+key, value, time.Second*duration).Err()
+}
+
+func SAdds[T []string | []any | []int64 | []int | []decimal.Decimal | []float64](key string, value T, duration time.Duration) error {
 	return goredis.SAdd(goredis_ctx, app_conf.Project+":"+key, value, time.Second*duration).Err()
 }
 
@@ -17,7 +22,18 @@ func SMemberMap(key string) (map[string]struct{}, error) {
 	return goredis.SMembersMap(goredis_ctx, app_conf.Project+":"+key).Result()
 }
 
-func IsMember(key, value string) (bool, error) {
-	return goredis.SIsMember(goredis_ctx, app_conf.Project+":"+key, value).Result()
+func IsMember(key string, value any) bool {
+	has, err := goredis.SIsMember(goredis_ctx, app_conf.Project+":"+key, value).Result()
+	if err != nil {
+		return false
+	}
+	return has
+}
 
+func IsMembers(key string, value ...any) []bool {
+	has, err := goredis.SMIsMember(goredis_ctx, app_conf.Project+":"+key, value).Result()
+	if err != nil {
+		return []bool{}
+	}
+	return has
 }
