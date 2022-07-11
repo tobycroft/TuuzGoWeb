261e155767d69474443688d15c607b8d2cf65b31
火线兔 <silverhawk@foxmail.com>
2022-07-11 13:00:46 +0800

    重构Redis模块

--- tuuz/Redis/SortSet.go
+++ tuuz/Redis/SortSet.go
@@ -2,14 +2,14 @@ package Redis
 
 import "github.com/go-redis/redis/v8"
 
-func ZADD(key string, score float64, value interface{}) error {
+func SortSet_Add(key string, score float64, value interface{}) error {
 	var z redis.Z
 	z.Score = score
 	z.Member = value
 	return goredis.ZAdd(goredis_ctx, key, &z).Err()
 }
 
-func ZCount(key string, min, max interface{}) int64 {
+func SortSet_Count(key string, min, max interface{}) int64 {
 	if min == nil || max == nil {
 		count, err := goredis.ZCard(goredis_ctx, key).Result()
 		if err != nil {
@@ -31,5 +31,16 @@ func ZCount(key string, min, max interface{}) int64 {
 		}
 		return count
 	}
+}
+
+func SortSet_Increase(key string, incr float64, value string) (float64, error) {
+	return goredis.ZIncrBy(goredis_ctx, key, incr, value).Result()
+}
+
+func SortSet_rank(key, rank_by string) (int64, error) {
+	return goredis.ZRank(goredis_ctx, key, rank_by).Result()
+}
+
+func SortSet_list() {
 
 }
