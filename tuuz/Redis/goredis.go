7de14f3362e32d510f5848a5fd859098e2f708f0
火线兔 <silverhawk@foxmail.com>
2022-07-13 13:39:39 +0800

    删除redis模块没必要的配置项

--- tuuz/Redis/goredis.go
+++ tuuz/Redis/goredis.go
@@ -3,7 +3,9 @@ package Redis
 import (
 	"context"
 	"github.com/go-redis/redis/v9"
+	"log"
 	"main.go/config/app_conf"
+	"time"
 )
 
 var goredis_ctx = context.Background()
@@ -11,15 +13,14 @@ var goredis_ctx = context.Background()
 var goredis *redis.Client
 
 func init() {
+	if !app_conf.Recicon_on {
+		return
+	}
 	options := redis.Options{
 		Addr:         app_conf.Redicon_address + ":" + app_conf.Redicon_port,
 		Username:     app_conf.Redicon_username,
 		Password:     app_conf.Redicon_password, // no password set
 		Network:      app_conf.Redicon_proto,
-		DialTimeout:  app_conf.Redicon_timeout,
-		PoolTimeout:  30,
-		ReadTimeout:  app_conf.Redicon_readtimeout,
-		WriteTimeout: app_conf.Redicon_writetimeout,
 		DB:           0, // use default DB
 		MinIdleConns: app_conf.Redicon_MinIdleConn,
 	}
@@ -27,4 +28,13 @@ func init() {
 		options.PoolSize = app_conf.Redicon_poolsize
 	}
 	goredis = redis.NewClient(&options)
+	go func() {
+		for {
+			time.Sleep(3 * time.Second)
+			ret, err := goredis.Ping(goredis_ctx).Result()
+			if err != nil {
+				log.Println("redis_out", ret, err)
+			}
+		}
+	}()
 }
