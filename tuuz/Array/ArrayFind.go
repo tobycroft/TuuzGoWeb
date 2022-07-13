7fcac6892deaea2e1b3ad093dda703a39daf2919
火线兔 <silverhawk@foxmail.com>
2022-07-13 15:56:56 +0800

    Array查找功能

--- tuuz/Array/ArrayFind.go
+++ tuuz/Array/ArrayFind.go
@@ -20,12 +20,12 @@ func ArrayFindMin[T int | int32 | int64 | float32 | float64](slice []T) T {
 	if len(slice) < 1 {
 		return 0
 	}
-	maxVal := slice[0]
+	minVal := slice[0]
 	maxIndex := 0
 	for i := 1; i < len(slice); i++ {
 		//从第二个 元素开始循环比较，如果发现有更小的，则交换
-		if maxVal > slice[i] {
-			maxVal = slice[i]
+		if minVal > slice[i] {
+			minVal = slice[i]
 			maxIndex = i
 		}
 	}
