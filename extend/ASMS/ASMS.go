6205b9c92d61f2616552119158f7c8d615317028
火线兔 <silverhawk@foxmail.com>
2022-07-14 10:26:28 +0800

    Asms更新

--- extend/ASMS/ASMS.go
+++ extend/ASMS/ASMS.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Unknwon/goconfig"
 	"github.com/tobycroft/Calc"
 	"main.go/tuuz/Jsong"
+	"main.go/tuuz/Log"
 	"main.go/tuuz/Net"
 	"time"
 )
@@ -21,7 +22,7 @@ import (
 
 const url = "http://asms.tuuz.cc:10081"
 
-func Sms_single(phone interface{}, quhao, text interface{}) error {
+func Sms_send(phone any, quhao, text any) error {
 	conf, err := goconfig.LoadConfigFile("conf.ini")
 	if err != nil {
 		return err
@@ -33,7 +34,7 @@ func Sms_single(phone interface{}, quhao, text interface{}) error {
 	name := value["name"]
 	token := value["token"]
 	ts := time.Now().Unix()
-	param := map[string]interface{}{
+	param := map[string]any{
 		"phone": phone,
 		"quhao": quhao,
 		"text":  text,
@@ -44,17 +45,52 @@ func Sms_single(phone interface{}, quhao, text interface{}) error {
 	ret, err := Net.Post(url+"/asms/send", nil, param, nil, nil)
 	//fmt.Println(ret, err)
 	if err != nil {
+		Log.Crrs(err, ret)
 		return err
 	} else {
-		rtt, errs := Jsong.JObject[string, interface{}](ret)
+		rtt, errs := Jsong.JObject[string, any](ret)
 		if errs != nil {
 			return errors.New(ret)
 		} else {
-			if rtt["code"] == 0 {
+			if Calc.Any2String(rtt["code"]) == "0" {
 				return nil
 			} else {
+				Log.Crrs(err, ret)
 				return errors.New(Calc.Any2String(rtt["echo"]))
 			}
 		}
+	}
+}
+
+func Sms_single(phone any, quhao, text any, code int64) error {
+	if len(Api_find_in1(phone)) > 0 {
+		return errors.New("你已经发送验证码，请稍后再次发送")
+	}
+	err := Sms_send(phone, quhao, text)
+	if err != nil {
+		return err
+	} else {
+		if !Api_insert(phone, code) {
+			return errors.New("验证码数据库插入失败")
+		}
+		return nil
+	}
+}
+
+func Sms_verify_in10(phone, code any) error {
+	data := Api_find_in10(phone, code)
+	if len(data) > 0 {
+		return nil
+	} else {
+		return errors.New("验证码出错")
+	}
+}
+
+func Sms_verify_in5(phone, code any) error {
+	data := Api_find_in10(phone, code)
+	if len(data) > 0 {
+		return nil
+	} else {
+		return errors.New("验证码出错")
 	}
 }
