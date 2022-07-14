e2013e9827382d062ca32891b1d046331ba182f1
火线兔 <silverhawk@foxmail.com>
2022-07-14 17:38:27 +0800

    Redis增加外部配置

--- config/app_conf/redis.go
+++ config/app_conf/redis.go
@@ -1,19 +1,40 @@
 package app_conf
 
-import "time"
+import (
+	"github.com/Unknwon/goconfig"
+	"time"
+)
 
-const Redicon_address = "10.0.0.171"
-const Redicon_port = "6379"
+var Redicon_address = "10.0.0.171"
+var Redicon_port = "6379"
+
 const Redicon_proto = "tcp"
 const Redicon_username = ""
 const Redicon_password = ""
 const Redicon_poolsize = 20
 const Redicon_MinIdleConn = 10
-const Recicon_on = false
+
+var Recicon_on = false
+
 const Recicon_panic_on_link_error = true
 const Recion_timeout_dial = 5 * time.Second
 const Recion_timeout_read = 5 * time.Second
 const Recion_timeout_write = 5 * time.Second
 const Recion_timeout_pool = 5 * time.Second
-
 const Recion_db = 0
+
+func init() {
+	cfg, err := goconfig.LoadConfigFile("conf.ini")
+	if err != nil {
+	} else {
+		value, err := cfg.GetSection("redis")
+		if err != nil {
+		} else {
+			if value["address"] != "" && value["address"] != "" && value["database"] != "" {
+				Redicon_address = value["address"]
+				Redicon_port = value["port"]
+				Recicon_on = true
+			}
+		}
+	}
+}
