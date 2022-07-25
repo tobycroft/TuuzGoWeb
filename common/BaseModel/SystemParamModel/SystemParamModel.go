51972f8e44e1817b90685c7c886c35dbac6ecdea
火线兔 <silverhawk@foxmail.com>
2022-07-25 11:15:50 +0800

    更新systemparammodel

--- common/BaseModel/SystemParamModel/SystemParamModel.go
+++ common/BaseModel/SystemParamModel/SystemParamModel.go
@@ -1,14 +1,15 @@
 package SystemParamModel
 
 import (
+	"github.com/tobycroft/gorose-pro"
 	"main.go/tuuz"
 	"main.go/tuuz/Log"
 )
 
-const table = "system_param"
+const Table = "system_param"
 
 func Api_find_val(key interface{}) interface{} {
-	db := tuuz.Db().Table(table)
+	db := tuuz.Db().Table(Table)
 	where := map[string]interface{}{
 		"key": key,
 	}
@@ -23,7 +24,7 @@ func Api_find_val(key interface{}) interface{} {
 }
 
 func Api_KV() map[string]string {
-	db := tuuz.Db().Table(table)
+	db := tuuz.Db().Table(Table)
 	ret, err := db.Get()
 	if err != nil {
 		Log.Dbrr(err, tuuz.FUNCTION_ALL())
@@ -34,5 +35,16 @@ func Api_KV() map[string]string {
 			arr[data["key"].(string)] = data["val"].(string)
 		}
 		return arr
+	}
+}
+
+func Api_select() []gorose.Data {
+	db := tuuz.Db().Table(Table)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
 	}
 }
