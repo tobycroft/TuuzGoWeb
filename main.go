d0a6720d6e81cd571a85d356b72b6b95b2c918ea
火线兔 <silverhawk@foxmail.com>
2023-02-12 10:28:03 +0800

    不使用端口判断，直接采用双端口阻塞方法

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/tobycroft/Calc"
 	"main.go/config/app_conf"
-	"main.go/extend/PortFinder"
 	"main.go/route"
 	"os"
 )
@@ -30,10 +29,7 @@ func main() {
 	mainroute.SetTrustedProxies([]string{"0.0.0.0/0"})
 	mainroute.SecureJsonPrefix(app_conf.SecureJsonPrefix)
 	route.OnRoute(mainroute)
-	if !PortFinder.PortInUse(80) {
-		mainroute.Run(":80")
-	} else {
-		mainroute.Run(":81")
-	}
+	mainroute.Run(":80")
+	mainroute.Run(":81")
 
 }
