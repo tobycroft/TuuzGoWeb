062618f2f637fd53531320109a3f1e45ceb3a1e2
火线兔 <silverhawk@foxmail.com>
2023-05-17 10:13:58 +0800

    Array方法增加any

--- tuuz/Array/Array.go
+++ tuuz/Array/Array.go
@@ -19,12 +19,13 @@ func ArrayUnique[T string | int | int32 | int64 | float32 | float64 | byte | dec
 	return n64
 }
 
-func ArrayDiff[T string | int | int32 | int64 | float32 | float64 | byte | decimal.Decimal | any](slice, slice2 []T) []T {
+func ArrayDiff[T string | int | int32 | int64 | float32 | float64 | byte | decimal.Decimal](slice, slice2 []T) []T {
 	n64 := []T{}
+
 	for _, s1 := range slice {
 		temp := true
 		for _, s2 := range slice2 {
-			if any(s2) == any(s1) {
+			if s2 == s1 {
 				temp = false
 				break
 			}
@@ -33,6 +34,7 @@ func ArrayDiff[T string | int | int32 | int64 | float32 | float64 | byte | decim
 			n64 = append(n64, s1)
 		}
 	}
+
 	return n64
 }
 
