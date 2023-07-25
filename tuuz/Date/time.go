789484f4935a520cfa72359cad75e0bf52fa536d
火线兔 <silverhawk@foxmail.com>
2023-07-25 11:52:20 +0800

    保持原有time

--- tuuz/Date/time.go
+++ tuuz/Date/time.go
@@ -8,20 +8,37 @@ import (
 )
 
 func Date_time_parser(date_time string, location *time.Location) (p time.Time, err error) {
-	p, err = date_parse_time(date_time)
-	if err != nil {
-		return
-	}
-	p = date_parse_offset(p, location)
-	return
-}
-
-func date_parse_time(date_time string) (p time.Time, err error) {
 	if strings.Contains(date_time, "T") {
 		if strings.Contains(date_time, ".") {
-			return time.Parse(time.RFC3339Nano, date_time)
+			p, err = time.Parse(time.RFC3339Nano, date_time)
+			if err != nil {
+				return p, err
+			}
+			if location != nil {
+				p = p.In(location)
+			} else {
+				loc, err := time.LoadLocation(app_conf.TimeZoneLocation)
+				if err != nil {
+					loc = app_conf.TimeZone
+				}
+				p = p.In(loc)
+			}
+			return p, err
 		} else {
-			return time.Parse(time.RFC3339, date_time)
+			p, err = time.Parse(time.RFC3339, date_time)
+			if err != nil {
+				return p, err
+			}
+			if location != nil {
+				p = p.In(location)
+			} else {
+				loc, err := time.LoadLocation(app_conf.TimeZoneLocation)
+				if err != nil {
+					loc = app_conf.TimeZone
+				}
+				p = p.In(loc)
+			}
+			return p, err
 		}
 	} else {
 		var datetime_exp *regexp.Regexp
@@ -35,17 +52,4 @@ func date_parse_time(date_time string) (p time.Time, err error) {
 			return time.Parse("2006-1-2", date_time)
 		}
 	}
-}
-
-func date_parse_offset(p time.Time, location *time.Location) time.Time {
-	if location != nil {
-		p = p.In(location)
-	} else {
-		loc, err := time.LoadLocation(app_conf.TimeZoneLocation)
-		if err != nil {
-			loc = app_conf.TimeZone
-		}
-		p = p.In(loc)
-	}
-	return p
 }
