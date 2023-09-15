1cd31768d4e461d85c40729f54fb5266ef5ce4d7
火线兔 <silverhawk@foxmail.com>
2023-09-15 18:01:32 +0800

    更新RedisPubSub

--- tuuz/Redis/Pubsub.go
+++ tuuz/Redis/Pubsub.go
@@ -2,23 +2,25 @@ package Redis
 
 import (
 	"context"
-	"fmt"
+	"github.com/bytedance/sonic"
+	"github.com/redis/go-redis/v9"
 	"main.go/config/app_conf"
 )
 
-func Suscribe(channel string) {
+type Pubsub struct {
+}
 
-	pubsub := goredis.Subscribe(context.Background(), app_conf.Project+":"+channel)
+func (self *Pubsub) Subscribe(channel string) <-chan *redis.Message {
+	return goredis.Subscribe(context.Background(), app_conf.Project+":"+channel).Channel()
+}
+func (self *Pubsub) Publish_string(channel string, message string) error {
+	return goredis.Publish(context.Background(), app_conf.Project+":"+channel, message).Err()
+}
 
-	_, err := pubsub.Receive(context.Background())
+func (self *Pubsub) Publish_struct(channel string, message_struct any) error {
+	message, err := sonic.MarshalString(message_struct)
 	if err != nil {
-		return
+		return err
 	}
-
-	ch := pubsub.Channel()
-
-	for msg := range ch {
-		fmt.Println(msg.Channel, msg.Payload)
-	}
-
+	return goredis.Publish(context.Background(), app_conf.Project+":"+channel, message).Err()
 }
