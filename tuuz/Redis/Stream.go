2fdbff89ce3ed00f30852041b0f2a1e62a69281b
火线兔 <silverhawk@foxmail.com>
2023-09-21 17:39:52 +0800

    新增Redis Stream

--- tuuz/Redis/Stream.go
+++ tuuz/Redis/Stream.go
@@ -6,9 +6,17 @@ import (
 	"main.go/config/app_conf"
 )
 
-func Stream_publish(stream_key string, value any) error {
+func Stream_publish(stream_channel string, value any) error {
 	var xa redis.XAddArgs
-	xa.Stream = app_conf.Project + ":" + stream_key
+	xa.Stream = app_conf.Project + ":" + stream_channel
 	xa.Values = value
 	return goredis.XAdd(context.Background(), &xa).Err()
+}
+
+func Stream_xLength(stream_channel string) (int64, error) {
+	return goredis.XLen(context.Background(), app_conf.Project+":"+stream_channel).Result()
+}
+
+func Stream_xRange(stream_key string) {
+
 }
